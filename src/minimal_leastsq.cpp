18b50109d803abe46eb04fea0d3317c1f45c4842
Ismael <ijimenezm@hotmail.es>
2016-06-04 22:00:02 +0200

    adapt to new benchmark implementation

--- src/minimal_leastsq.cpp
+++ src/minimal_leastsq.cpp
@@ -13,92 +13,63 @@
 // limitations under the License.
 
 #include "minimal_leastsq.h"
-#include <math.h>
+#include <algorithm>
+#include <cmath>
 #include "assert.h"
-
-enum BigO {
-  o1,
-  oN,
-  oNSquared,
-  oNCubed,
-  oLogN,
-  oNLogN
-};
 
 // Internal function to calculate the different scalability forms
 std::function<double(int)> FittingCurve(BigO complexity) {
   switch (complexity) {
-  case oN:
-    return [](int n) {return n; };
-  case oNSquared:
-    return [](int n) {return n*n; };
-  case oNCubed:
-    return [](int n) {return n*n*n; };
-  case oLogN:
-    return [](int n) {return log2(n); };
-  case oNLogN:
-    return [](int n) {return n * log2(n); };
-  case o1:
-  default:
-    return [](int) {return 1; };
+    case oN:
+      return [](int n) -> double { return n; };
+    case oNSquared:
+      return [](int n) -> double { return n * n; };
+    case oNCubed:
+      return [](int n) -> double { return n * n * n; };
+    case oLogN:
+      return [](int n) { return std::log2(n); };
+    case oNLogN:
+      return [](int n) { return n * std::log2(n); };
+    case o1:
+    default:
+      return [](int) { return 1.0; };
   }
 }
 
-// Internal function to to return an string for the calculated complexity (only on automatic mode)
+// Function to return an string for the calculated complexity
 std::string GetBigOString(BigO complexity) {
   switch (complexity) {
-  case oN:
-    return "* N";
-  case oNSquared:
-    return "* N**2";
-  case oNCubed:
-    return "* N**3";
-  case oLogN:
-    return "* lgN";
-  case oNLogN:
-    return "* NlgN";
-  case o1:
-    return "* 1";
-  default:
-    return "";
+    case oN:
+      return "N";
+    case oNSquared:
+      return "N^2";
+    case oNCubed:
+      return "N^3";
+    case oLogN:
+      return "lgN";
+    case oNLogN:
+      return "NlgN";
+    case o1:
+      return "(1)";
+    default:
+      return "f(N)";
   }
 }
 
-// Find the coefficient for the for the best fitting high-order term in the running time, by minimizing the sum of squares of relative error.
-//   - n          : Vector containing the size of the benchmark tests.
-//   - time       : Vector containing the times for the benchmark tests.
-
-LeastSq AutoMinimalLeastSq(const std::vector<int>& n, const std::vector<double>& time) {
-  assert(n.size() == time.size());
-  assert(n.size() >= 2);  // Do not compute fitting curve is less than two benchmark runs are given
-
-  std::vector<BigO> fit_curves = { oLogN, oN, oNLogN, oNSquared, oNCubed };
-
-  LeastSq best_fit = MinimalLeastSq(n, time, FittingCurve(o1)); // Take o1 as default best fitting curve
-  best_fit.complexity = GetBigOString(o1);
-
-  // Compute all possible fitting curves and stick to the best one
-  for (const auto& fit : fit_curves) {
-    LeastSq current_fit = MinimalLeastSq(n, time, FittingCurve(fit));
-    if (current_fit.rms < best_fit.rms) {
-      best_fit = current_fit;
-      best_fit.complexity = GetBigOString(fit);
-    }
-  }
-
-  return best_fit;
-}
-
-// Find the coefficient for the high-order term in the running time, by minimizing the sum of squares of relative error, for the fitting curve given on the lambda expresion.
+// Find the coefficient for the high-order term in the running time, by
+// minimizing the sum of squares of relative error, for the fitting curve
+// given by the lambda expresion.
 //   - n             : Vector containing the size of the benchmark tests.
 //   - time          : Vector containing the times for the benchmark tests.
 //   - fitting_curve : lambda expresion (e.g. [](int n) {return n; };).
 
-LeastSq MinimalLeastSq(const std::vector<int>& n, const std::vector<double>& time, std::function<double(int)> fitting_curve) {
-  double sigma_gn = 0;
-  double sigma_gn_squared = 0;
-  double sigma_time = 0;
-  double sigma_time_gn = 0;
+LeastSq MinimalLeastSq(const std::vector<int>& n,
+                       const std::vector<double>& time,
+                       std::function<double(int)> fitting_curve) {
+  double sigma_gn = 0.0;
+  double sigma_gn_squared = 0.0;
+  double sigma_time = 0.0;
+  double sigma_time_gn = 0.0;
 
   // Calculate least square fitting parameter
   for (size_t i = 0; i < n.size(); ++i) {
@@ -110,20 +81,60 @@ LeastSq MinimalLeastSq(const std::vector<int>& n, const std::vector<double>& tim
   }
 
   LeastSq result;
+  result.complexity = oLambda;
 
   // Calculate complexity.
   result.coef = sigma_time_gn / sigma_gn_squared;
 
   // Calculate RMS
-  double rms = 0;
+  double rms = 0.0;
   for (size_t i = 0; i < n.size(); ++i) {
     double fit = result.coef * fitting_curve(n[i]);
     rms += pow((time[i] - fit), 2);
   }
 
+  // Normalized RMS by the mean of the observed values
   double mean = sigma_time / n.size();
-
-  result.rms = sqrt(rms / n.size()) / mean; // Normalized RMS by the mean of the observed values
+  result.rms = sqrt(rms / n.size()) / mean;
 
   return result;
+}
+
+// Find the coefficient for the high-order term in the running time, by
+// minimizing the sum of squares of relative error.
+//   - n          : Vector containing the size of the benchmark tests.
+//   - time       : Vector containing the times for the benchmark tests.
+//   - complexity : If different than oAuto, the fitting curve will stick to
+//                  this one. If it is oAuto, it will be calculated the best
+//                  fitting curve.
+LeastSq MinimalLeastSq(const std::vector<int>& n,
+                       const std::vector<double>& time, const BigO complexity) {
+  assert(n.size() == time.size());
+  assert(n.size() >= 2);  // Do not compute fitting curve is less than two
+                          // benchmark runs are given
+  assert(complexity != oNone);
+
+  LeastSq best_fit;
+
+  if (complexity == oAuto) {
+    std::vector<BigO> fit_curves = {oLogN, oN, oNLogN, oNSquared, oNCubed};
+
+    // Take o1 as default best fitting curve
+    best_fit = MinimalLeastSq(n, time, FittingCurve(o1));
+    best_fit.complexity = o1;
+
+    // Compute all possible fitting curves and stick to the best one
+    for (const auto& fit : fit_curves) {
+      LeastSq current_fit = MinimalLeastSq(n, time, FittingCurve(fit));
+      if (current_fit.rms < best_fit.rms) {
+        best_fit = current_fit;
+        best_fit.complexity = fit;
+      }
+    }
+  } else {
+    best_fit = MinimalLeastSq(n, time, FittingCurve(complexity));
+    best_fit.complexity = complexity;
+  }
+
+  return best_fit;
 }
--- test/minimal_leastsq_test.cpp
+++ test/minimal_leastsq_test.cpp
@@ -1,220 +1,217 @@
 #include <algorithm>
 
-#include "minimal_leastsq.h"
 #include "gtest/gtest.h"
+#include "minimal_leastsq.h"
 
-TEST(minimal_leastsq_test, constant)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 7, 6, 8, 7 };
+TEST(minimal_leastsq_test, constant) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{7, 6, 8, 7};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int) {return 1;});
-  
+  LeastSq result = MinimalLeastSq(length, time, [](int) { return 1.0; });
+
   EXPECT_NEAR(result.coef, 7, 0.01);
   EXPECT_NEAR(result.rms, 0.101, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oLambda);
 }
 
-TEST(minimal_leastsq_test, constant_good_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 5, 5, 5, 5 };
+TEST(minimal_leastsq_test, constant_good_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{5, 5, 5, 5};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int) {return 1;});
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::o1);
+
   EXPECT_NEAR(result.coef, 5, 0.01);
   EXPECT_NEAR(result.rms, 0, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::o1);
 }
 
-TEST(minimal_leastsq_test, linear)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 4, 3, 9, 13 };
+TEST(minimal_leastsq_test, linear) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{4, 3, 9, 13};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return n;});
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oN);
+
   EXPECT_NEAR(result.coef, 2.96, 0.01);
   EXPECT_NEAR(result.rms, 0.228, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oN);
 }
 
-TEST(minimal_leastsq_test, linear_good_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 3, 6, 9, 12 };
+TEST(minimal_leastsq_test, linear_good_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{3, 6, 9, 12};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return n;});
-  
+  LeastSq result =
+      MinimalLeastSq(length, time, [](int n) -> double { return n; });
+
   EXPECT_NEAR(result.coef, 3, 0.01);
   EXPECT_NEAR(result.rms, 0, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oLambda);
 }
 
-TEST(minimal_leastsq_test, N_squared)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 6, 5, 7, 10 };
+TEST(minimal_leastsq_test, N_squared) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{6, 5, 7, 10};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return n*n;});
-  
+  LeastSq result =
+      MinimalLeastSq(length, time, [](int n) -> double { return n * n; });
+
   EXPECT_NEAR(result.coef, 0.703, 0.01);
   EXPECT_NEAR(result.rms, 0.421, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oLambda);
 }
 
-TEST(minimal_leastsq_test, N_squared_good_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 1, 4, 9, 16 };
+TEST(minimal_leastsq_test, N_squared_good_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{1, 4, 9, 16};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return n*n;});
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oNSquared);
+
   EXPECT_NEAR(result.coef, 1, 0.01);
   EXPECT_NEAR(result.rms, 0, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oNSquared);
 }
 
-TEST(minimal_leastsq_test, N_cubed)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 4, 35, 150, 390 };
+TEST(minimal_leastsq_test, N_cubed) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{4, 35, 150, 390};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return n*n*n;});
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oNCubed);
+
   EXPECT_NEAR(result.coef, 5.99, 0.01);
   EXPECT_NEAR(result.rms, 0.064, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oNCubed);
 }
 
-TEST(minimal_leastsq_test, N_cubed_good_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 8, 64, 216, 512 };
+TEST(minimal_leastsq_test, N_cubed_good_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{8, 64, 216, 512};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return n*n*n;});
-  
+  LeastSq result =
+      MinimalLeastSq(length, time, [](int n) -> double { return n * n * n; });
+
   EXPECT_NEAR(result.coef, 8, 0.01);
   EXPECT_NEAR(result.rms, 0, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oLambda);
 }
 
-TEST(minimal_leastsq_test, logN)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 3, 22, 33, 44 };
+TEST(minimal_leastsq_test, logN) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{3, 22, 33, 44};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return log2(n);});
-  
+  LeastSq result =
+      MinimalLeastSq(length, time, [](int n) { return std::log2(n); });
+
   EXPECT_NEAR(result.coef, 21.605, 0.01);
   EXPECT_NEAR(result.rms, 0.065, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oLambda);
 }
 
-TEST(minimal_leastsq_test, logN_good_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 0, 8, 12, 16 };
+TEST(minimal_leastsq_test, logN_good_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{0, 8, 12, 16};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return log2(n);});
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oLogN);
+
   EXPECT_NEAR(result.coef, 7.85, 0.01);
   EXPECT_NEAR(result.rms, 0.03, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oLogN);
 }
 
-TEST(minimal_leastsq_test, N_logN)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 5, 40, 97, 180 };
+TEST(minimal_leastsq_test, N_logN) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{5, 40, 97, 180};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return n * log2(n);});
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oNLogN);
+
   EXPECT_NEAR(result.coef, 21.865, 0.01);
   EXPECT_NEAR(result.rms, 0.066, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oNLogN);
 }
 
-TEST(minimal_leastsq_test, N_logN_good_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 0, 43, 102, 172 };
+TEST(minimal_leastsq_test, N_logN_good_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{0, 43, 102, 172};
 
-  LeastSq result = MinimalLeastSq(length, time, [](int n) {return n * log2(n);});
-  
+  LeastSq result =
+      MinimalLeastSq(length, time, [](int n) { return n * std::log2(n); });
+
   EXPECT_NEAR(result.coef, 21.487, 0.01);
   EXPECT_NEAR(result.rms, 0.002, 0.01);
-  EXPECT_EQ(result.complexity, "");
+  EXPECT_EQ(result.complexity, BigO::oLambda);
 }
 
-TEST(minimal_leastsq_test, constant_auto_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 5, 5, 5, 5 };
+TEST(minimal_leastsq_test, constant_auto_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{5, 5, 5, 5};
 
-  LeastSq result = AutoMinimalLeastSq(length, time);
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oAuto);
+
   EXPECT_NEAR(result.coef, 5, 0.01);
   EXPECT_NEAR(result.rms, 0, 0.01);
-  EXPECT_EQ(result.complexity, "* 1");
+  EXPECT_EQ(result.complexity, BigO::o1);
 }
 
-TEST(minimal_leastsq_test, linear_auto_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 3, 6, 9, 12 };
+TEST(minimal_leastsq_test, linear_auto_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{3, 6, 9, 12};
 
-  LeastSq result = AutoMinimalLeastSq(length, time);
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oAuto);
+
   EXPECT_NEAR(result.coef, 3, 0.01);
   EXPECT_NEAR(result.rms, 0, 0.01);
-  EXPECT_EQ(result.complexity, "* N");
+  EXPECT_EQ(result.complexity, BigO::oN);
 }
 
-TEST(minimal_leastsq_test, N_squared_auto_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 1, 4, 9, 16 };
+TEST(minimal_leastsq_test, N_squared_auto_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{1, 4, 9, 16};
 
-  LeastSq result = AutoMinimalLeastSq(length, time);
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oAuto);
+
   EXPECT_NEAR(result.coef, 1, 0.01);
   EXPECT_NEAR(result.rms, 0, 0.01);
-  EXPECT_EQ(result.complexity, "* N**2");
+  EXPECT_EQ(result.complexity, BigO::oNSquared);
 }
 
-TEST(minimal_leastsq_test, N_cubed_auto_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 8, 64, 216, 512 };
+TEST(minimal_leastsq_test, N_cubed_auto_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{8, 64, 216, 512};
 
-  LeastSq result = AutoMinimalLeastSq(length, time);
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oAuto);
+
   EXPECT_NEAR(result.coef, 8, 0.01);
   EXPECT_NEAR(result.rms, 0, 0.01);
-  EXPECT_EQ(result.complexity, "* N**3");
+  EXPECT_EQ(result.complexity, BigO::oNCubed);
 }
 
-TEST(minimal_leastsq_test, logN_cubed_auto_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 0, 8, 12, 16 };
+TEST(minimal_leastsq_test, logN_cubed_auto_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{0, 8, 12, 16};
 
-  LeastSq result = AutoMinimalLeastSq(length, time);
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oAuto);
+
   EXPECT_NEAR(result.coef, 7.85, 0.01);
   EXPECT_NEAR(result.rms, 0.03, 0.01);
-  EXPECT_EQ(result.complexity, "* lgN");
+  EXPECT_EQ(result.complexity, BigO::oLogN);
 }
 
-TEST(minimal_leastsq_test, N_logN_cubed_auto_fit)
-{  
-  std::vector<int> length{ 1, 2, 3, 4 };
-  std::vector<double> time{ 0, 43, 102, 172 };
+TEST(minimal_leastsq_test, N_logN_cubed_auto_fit) {
+  std::vector<int> length{1, 2, 3, 4};
+  std::vector<double> time{0, 43, 102, 172};
 
-  LeastSq result = AutoMinimalLeastSq(length, time);
-  
+  LeastSq result = MinimalLeastSq(length, time, BigO::oAuto);
+
   EXPECT_NEAR(result.coef, 21.487, 0.01);
   EXPECT_NEAR(result.rms, 0.002, 0.01);
-  EXPECT_EQ(result.complexity, "* NlgN");
+  EXPECT_EQ(result.complexity, BigO::oNLogN);
+}
+
+TEST(minimal_leastsq_test, Big_O_String) {
+  EXPECT_EQ("(1)", GetBigOString(BigO::o1));
+  EXPECT_EQ("N", GetBigOString(BigO::oN));
+  EXPECT_EQ("N^2", GetBigOString(BigO::oNSquared));
+  EXPECT_EQ("N^3", GetBigOString(BigO::oNCubed));
+  EXPECT_EQ("lgN", GetBigOString(BigO::oLogN));
+  EXPECT_EQ("NlgN", GetBigOString(BigO::oNLogN));
+  EXPECT_EQ("f(N)", GetBigOString(BigO::oLambda));
 }
